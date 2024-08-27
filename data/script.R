28fe42b370f09984c382a3a2320c92680f8c86f7
cafecomdados <93016130+cafecomdados@users.noreply.github.com>
2024-08-27 08:57:02 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -2,23 +2,19 @@ library(dplyr)
 library(readxl)
 library(lubridate)
 
-cota08a23 <- readRDS('data/cota08a23.rds')
+cota19a23 <- readRDS('data/cota19a23.rds')
 
 url24 <- "https://www.camara.leg.br/cotas/Ano-2024.xlsx"
 arquivo_local <- "Ano-2024.xlsx"
 download.file(url24, destfile = arquivo_local, mode = "wb")
 
 cota24 <- read_excel(arquivo_local) |> 
-  filter(sgUF == 'SE')
+  select(txNomeParlamentar, nuLegislatura, sgUF, sgPartido, txtDescricao, txtDescricaoEspecificacao, txtFornecedor, txtCNPJCPF, datEmissao, vlrLiquido, numAno, urlDocumento) |> 
+  mutate(datEmissao = as.Date(datEmissao, format = "%Y-%m-%d"))
 
 file.remove('Ano-2024.xlsx')
 
-cota <- cota08a23 |> 
-  mutate(cpf = as.numeric(gsub("\\.", "", cpf))) |> 
-  full_join(cota24) |> 
-  mutate(data = ymd_hms(datEmissao))
-
-cota <- cota |> 
-  select(txNomeParlamentar, nuLegislatura, sgUF, sgPartido, txtDescricao, txtDescricaoEspecificacao, txtFornecedor, txtCNPJCPF, datEmissao, vlrLiquido, numAno, urlDocumento, data)
+cota <- cota19a23 |> 
+  full_join(cota24) 
 
 saveRDS(cota, 'data/cota.rds')
