71778dfee8afd1e2128c1be41ffa89f2a471339c
cafecomdados <93016130+cafecomdados@users.noreply.github.com>
2024-08-28 20:50:22 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -76,6 +76,7 @@ cota <- cota |>
     txtDescricao == "TELEFONIA" ~ "Telefonia",
     txtDescricao == "AQUISIÇÃO DE TOKENS E CERTIFICADOS DIGITAIS" ~ "Tokens e Certificados Digitais",
     TRUE ~ txtDescricao 
-  )) 
+  )) |> 
+  mutate(urlDocumento = str_replace(urlDocumento, "https://www.camara.leg.br/cota-parlamentar/", ""))
 
 saveRDS(cota, 'data/cota.rds')
